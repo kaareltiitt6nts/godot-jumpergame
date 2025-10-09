34d4fed23f6bda95dcf130464df9451199e80cad
kaareltiitt6nts <kaarel-tiit.tonts@voco.ee>
2025-10-09 11:44:34 +0300

    gameover mainmenu parandused

--- scripts/game_over.gd
+++ scripts/game_over.gd
@@ -1,15 +1,16 @@
-extends Control
+extends Node2D
 
 @export var exit_url : String = "https://www.voco.ee"
 
 func _ready() -> void:
 	Levelmanager.deleteLevel()
 
-func _on_exit_button_pressed() -> void:
-	print("Opening URL: ", exit_url)
-	OS.shell_open(exit_url)
-
-func _on_restart_button_pressed() -> void:
+func _on_play_button_pressed() -> void:
 	print("Restarting game...")
 	queue_free()
 	get_tree().change_scene_to_file("res://scenes/levels/main_menu.tscn")
+
+
+func _on_homepage_button_pressed() -> void:
+	print("Opening URL: ", exit_url)
+	OS.shell_open(exit_url)
